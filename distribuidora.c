05088762163539d0a3634bf4cfdaa7351610fc81
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-04-28 00:19:58 -0300

    Punto 3 completado

--- distribuidora.c
+++ distribuidora.c
@@ -20,14 +20,15 @@ Producto *Productos;   //El tamaño de este arreglo depende de la variable
 
 }Cliente;
 
-
+float costoTotal(Producto p);
 
 int main(){
 
 srand(time(NULL));
 int cantClientes;
-do{
-   printf("----Ingrese la cantidad de clientes (1-5)----\n");
+do{ 
+    fflush(stdin);
+    printf("----Ingrese la cantidad de clientes (1-5)----\n");
     scanf("%i", &cantClientes); 
     
  }while((cantClientes<1) || (cantClientes>5));
@@ -39,7 +40,7 @@ for(int i=0;i<cantClientes;i++){
     printf("Ingrese el nombre del cliente: %i\n", i+1);
     char buff[50];
     scanf("%s", buff);
-    Clientes[i].NombreCliente = (char *) malloc(sizeof(char) * sizeof(strlen(buff)+1));
+    Clientes[i].NombreCliente = (char *) malloc(sizeof(char) * (strlen(buff)+1));
     strcpy(Clientes[i].NombreCliente,buff);
     int cantProd = (rand() % 4) + 1;
     Clientes[i].CantidadProductosAPedir=cantProd;
@@ -49,8 +50,7 @@ for(int i=0;i<cantClientes;i++){
         Clientes[i].Productos[j].ProductoID=j+1;
         int cantTipoP = (rand()%10) + 1;
         Clientes[i].Productos[j].Cantidad = cantTipoP;
-        int tipoP = (rand()%5) +1;
-
+        int tipoP = (rand()%5);
         Clientes[i].Productos[j].TipoProducto = TiposProductos[tipoP];
         float precio = (rand() %91) + 10;
         Clientes[i].Productos[j].PrecioUnitario=precio;
@@ -58,10 +58,36 @@ for(int i=0;i<cantClientes;i++){
 
 
 }
+printf("\n----CLIENTES----\n");
+for(int i=0;i<cantClientes;i++){
+    printf("Cliente: %s\n", Clientes[i].NombreCliente);
+    printf("ID: %i\n", Clientes[i].ClienteID);
+    printf("Cantidad de tipos de producto pedidos: %i\n", Clientes[i].CantidadProductosAPedir);
 
-
+    printf("---Productos pedidos---\n");
+    float TotalGastado=0;
+    for(int j=0;j<Clientes[i].CantidadProductosAPedir;j++){
+        printf("ID: %i\n", Clientes[i].Productos[j].ProductoID);
+        printf("Cantidad: %i\n", Clientes[i].Productos[j].Cantidad);
+        printf("Tipo de producto: %s\n", Clientes[i].Productos[j].TipoProducto);
+        printf("Precio unitario: %.0f\n", Clientes[i].Productos[j].PrecioUnitario);
+        float subTotal = costoTotal(Clientes[i].Productos[j]);
+        TotalGastado+=subTotal;
+        printf("Total por producto: %.0f\n", subTotal);
+        printf("--------------\n");
+        
+    }
+    printf("Monto total del cliente: %.0f\n", TotalGastado);
+    printf("\n-------------------\n");
+}
 
 
 return 0;
 
+}
+
+
+float costoTotal(Producto p){
+    float Total = p.Cantidad * p.PrecioUnitario;
+    return Total;
 }
